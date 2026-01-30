3a41bb3a14dffa8f42bf55f0559a58835a69f9b0
Gaya21914026 <assamgaya2001@gmail.com>
2026-01-30 17:27:21 +0100

    eval

--- app/routers/cvs.py
+++ app/routers/cvs.py
@@ -15,19 +15,3 @@ async def chat_with_ai(data: ChatRequest):
     ai_message = await openrouter_chat(data.message, data.role) 
     return ChatResponse(response=ai_message )
 
-@router.post("/summarize_with_ia")
-async def summarize_cv(file: UploadFile):
-    
-    result = save(file)
-    images = result["images"]
-
-    # 2. Prendre la première image
-    first_image = images[0]
-
-    # 3. Envoyer à l’IA
-    summary = await openrouter_chat(
-        message="que vois tu sur cette image?",
-        image_path=first_image
-    )
-
-    return {"summary": summary}
